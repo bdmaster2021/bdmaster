966a1677447fd8ab1640e688c31d8bcc7620e0db
bdmaster2021 <81631894+bdmaster2021@users.noreply.github.com>
2021-03-30 22:02:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text=str(time.time()))
+        message = TextSendMessage(text='金門好運到')
         line_bot_api.reply_message(event.reply_token, message)
 
 import os
