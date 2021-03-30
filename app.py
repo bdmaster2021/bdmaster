dd401f88622364a96553fceaea8bc3c86c565e43
joe1230102 <62791136+joe1230102@users.noreply.github.com>
2021-03-31 00:12:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import time
+import urllib.request
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -68,10 +69,12 @@ def handle_message(event):
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
     else:
-        message = TextSendMessage(text='金門好運到台灣!')
-        url = "https://github.com/bdmaster2021/bdmaster/blob/main/text.txt"
-        file = urllib.request.urlopen(url)
-        message = file.read()
+        #message = TextSendMessage(text='金門好運到台灣!')
+
+        #url = "https://raw.githubusercontent.com/bdmaster2021/bdmaster/main/text.txt"
+        #file = urllib.request.urlopen(url)
+        f = open('text.txt', 'r')
+        message = TextSendMessage(text=f)#file.read().decode("utf-8"))
         line_bot_api.reply_message(event.reply_token, message)
 
 import os
