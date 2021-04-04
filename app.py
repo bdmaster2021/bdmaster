b6b73b626388ecbe1ed394406cbd6ce2ba63eb26
joe1230102 <62791136+joe1230102@users.noreply.github.com>
2021-04-04 21:50:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    id = event.source.userId
     if '開始使用選才機器人' in msg:
-        message = start_message()
+        message = TextSendMessage(text='ID:'+str(id)+'%0D%0A使用說明：%0D%0A請輸入您的科系(含組)全名')
         line_bot_api.reply_message(event.reply_token, message)
     elif '最新合作廠商' in msg:
         message = imagemap_message()
