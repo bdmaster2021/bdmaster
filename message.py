c08d16e1109d79c3a2c45f731064268123336753
joe1230102 <62791136+joe1230102@users.noreply.github.com>
2021-04-04 23:52:47 +0800

    Update message.py

--- message.py
+++ message.py
@@ -12,7 +12,7 @@ def imagemap_message():
         actions=[
             URIImagemapAction(
                 #性別
-                text='性別'
+                text='性別',
                 area=ImagemapArea(
                     x=0, y=0, width=2000, height=1000
                 )
@@ -20,14 +20,14 @@ def imagemap_message():
            
             URIImagemapAction(
                 #區域
-                text='區域'
+                text='區域',
                 area=ImagemapArea(
                     x=0, y=1000, width=1000, height=1000
                 )
             ),
             URIImagemapAction(
                 #競爭校系
-                text='競爭校系'
+                text='競爭校系',
                 area=ImagemapArea(
                     x=1000, y=1000, width=1000, height=1000
                 )
