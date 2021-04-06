2841f573b5368c6f2d7f1b51b56ee5f998bc4e2a
joe1230102 <62791136+joe1230102@users.noreply.github.com>
2021-04-07 00:14:51 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', minute='*/20')
 def scheduled_job():
-    url = 'https://bdmaster2021.herokuapp.com'
+    url = 'https://bdmaster2021.herokuapp.com/'
     conn = urllib.request.urlopen(url)
         
     for key, value in conn.getheaders():
