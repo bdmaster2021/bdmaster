5a9aef9d24df3940f7587ae93f958de1431e3128
joe1230102 <62791136+joe1230102@users.noreply.github.com>
2021-04-08 16:16:50 +0800

    Update message.py

--- message.py
+++ message.py
@@ -53,9 +53,9 @@ def majorsCom_message(id):
     data = f.read().split(',')
     indexTemp = data.index(majorIndex)
     msg = '第一名:'+data[indexTemp+1]+'\n第二名:'+data[indexTemp+2]+'\n第三名:'+data[indexTemp+3]+'\n第四名:'+data[indexTemp+4]+'\n第五名:'+data[indexTemp+5]+'\n'
-    message = TextSendMessage(text=msg)
+    #message = TextSendMessage(text=msg)
     f.close
-    return message
+    return msg
 
 #ImagemapSendMessage(組圖訊息)
 def imagemap_message(majorIndex):
